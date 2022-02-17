a94fcad30188c57e49cc333bbb667455765f1dfe
ShunyaCodes <patilabhilash1046@gmail.com>
2022-02-17 00:09:57 -0800

    Added Custom Router to the Project

---
+++ lib/config/custom_router.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class CustomRouter {
+  static Route onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(
+          settings: const RouteSettings(name: '/'),
+          builder: (_) => const Scaffold(),
+        );
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route _errorRoute() {
+    return MaterialPageRoute(
+      builder: (_) => Scaffold(
+        appBar: AppBar(
+          title: const Text("Error"),
+        ),
+        body:const Center(child: Text("Something Went Wrong!"),) ,
+      ),
+    );
+  }
+}
